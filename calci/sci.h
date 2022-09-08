752c4321b7a80f12784b91857caccc02017271c4
Mat-Fraser <86045021+Mat-Fraser@users.noreply.github.com>
2022-09-08 17:19:42 +0530

    Add files via upload

---
+++ calci/sci.h
@@ -0,0 +1,23 @@
+#ifndef SCIENTIFIC_H
+#define SCIENTIFIC_H
+
+#include <string>
+#include <array>
+#include <cmath>
+#include "calci.h"
+
+class Scientific : public Calculator
+{
+public:
+    Scientific();     
+    virtual ~Scientific() = default; 
+    virtual void welcome() const override;        
+    virtual void parseOperation(const std::string& input) override;
+    void sin();
+    void cos();
+    void tan();
+    void ln();
+    void log();
+    void abs();
+    void pow();
+};
